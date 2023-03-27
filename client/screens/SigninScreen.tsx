c71aec6c94bcd31a0da16d964252a527fa8e8ade
nhlong1512 <20521569@gm.uit.edu.vn>
2023-03-27 20:47:09 +0700

    Add Login UI

--- client/screens/SigninScreen.tsx
+++ client/screens/SigninScreen.tsx
@@ -1,3 +1,4 @@
+<script src="http://localhost:8097"></script>;
 import React, { useState } from "react";
 import { SafeAreaView, StyleSheet, Image, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
@@ -6,69 +7,68 @@ import { TextInput, Button } from "react-native-paper";
 const SigninScreen = () => {
   const [text, setText] = useState("");
   return (
-    <SafeAreaView className="flex-1 px-[20px] pt-[25px] flex justify-between mb-[20px]">
+    <SafeAreaView className="flex-1 px-[20px] pt-[25px] flex justify-between mb-[25px]">
       <StatusBar style="light" />
       <View>
-        <Text className="text-[32px] font-[400] text-[#303030] mt-[75px] leading-[45px]">
-          Welcome!
-        </Text>
-        <Text className="text-[16px] leading-[20px] font-[300] text-[#505050]">
-          Please sign in to continue
-        </Text>
-        <Text className="text-[#303030] text-[18px] leading-[22px] font-[400] mt-[38px]">
-          Email
+        <View>
+          <Image source={require("../assets/images/logo1.png")} />
+        </View>
+        <Text className="text-[24px] font-[700] text-[#6667AB] leading-[32px] text-center mb-[14px]">
+          ĐĂNG NHẬP
         </Text>
         <TextInput
           // value={text}
           // onChangeText={(text) => setText(text)}
-          className="mt-[8px] rounded-[5px]"
-          outlineColor="#969393"
-          activeOutlineColor="#505050"
+          className="mt-[8px] rounded-[10px]"
+          theme={{ roundness: 10 }}
+          outlineColor="transparent"
+          activeOutlineColor="#6667AB"
           placeholderTextColor="#969393"
           mode="outlined"
-          placeholder="example@gmail.com"
-          right={
-            <TextInput.Icon
-              icon={require("../assets/icons/Mail_outline.png")}
-            />
+          placeholder="Tên đăng nhập"
+          left={
+            <TextInput.Icon icon={require("../assets/icons/person_2.png")} />
           }
         />
-        <Text className="text-[#303030] text-[18px] leading-[22px] font-[400] mt-[23px]">
-          Password
-        </Text>
         <TextInput
           // value={text}
           // onChangeText={(text) => setText(text)}
           secureTextEntry={true}
-          className="mt-[8px] rounded-[5px]"
-          outlineColor="#969393"
-          activeOutlineColor="#505050"
+          className="mt-[24px] rounded-[10px]"
+          theme={{ roundness: 10 }}
+          outlineColor="transparent"
+          activeOutlineColor="#6667AB"
           placeholderTextColor="#969393"
           mode="outlined"
-          placeholder="••••••••••••••••"
-          right={<TextInput.Icon icon="eye" />}
+          placeholder="Mật khẩu"
+          left={<TextInput.Icon icon={require("../assets/icons/lock.png")} />}
         />
         <View className="flex justify-between items-center flex-row mt-[8px]">
-          <Text className="text-[#969393]">
-            <Image source={require("../assets/icons/Check_circle.png")} />
-            &nbsp; Remember password
+          <Text>
+            <Image source={require("../assets/icons/check_circle_2.png")} />
+            &nbsp;
+            <Text className="text-[#969393] underline">Lưu mật khẩu</Text>
           </Text>
-          <Text className="text-[#969393]">Forgot password?</Text>
+          <Text className="text-[#969393] underline">Quên mật khẩu?</Text>
         </View>
-        <Button
-          // icon="camera"
-          mode="contained"
-          compact={true}
-          className="rounded-[10px] px-[32px] bg-[#EC7160] mt-[48px]"
-          onPress={() => console.log("Pressed")}
-        >
-          Sign In
-        </Button>
+        <View className="">
+          <Button
+            // icon="camera"
+            mode="contained"
+            compact={true}
+            className="rounded-[10px] py-[4px] bg-[#6667AB] mt-[48px]"
+            onPress={() => console.log("Pressed")}
+          >
+            <Image source={require("../assets/icons/telegram_icon.png")} />
+            <Text className="text-[18px] font-[700]">&nbsp; ĐĂNG NHẬP</Text>
+          </Button>
+        </View>
+        <Text className="text-center flex flex-row justify-center items-center mt-[24px] text-[16px] leading-[20px]">
+          Chưa có tài khoản?
+          <Text> </Text>
+          <Text className="font-[500] underline">Đăng Ký</Text>
+        </Text>
       </View>
-
-      <Text className="text-center flex flex-row justify-center items-center mt-[48px] text-[16px] leading-[20px]">
-        Don't have an account yet? Sign up
-      </Text>
     </SafeAreaView>
   );
 };
