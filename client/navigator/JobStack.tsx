e95559b527ea3b261b0a9cb2a8b440af3721e323
nhlong1512 <20521569@gm.uit.edu.vn>
2023-06-30 11:30:53 +0700

    Render jobDetail

--- client/navigator/JobStack.tsx
+++ client/navigator/JobStack.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AddJobScreen from "../screens/ManageJob/AddJobScreen";
 import JobPostedScreen from "../screens/ManageJob/JobPostedScreen";
+import JobDetailScreen from "../screens/ManageJob/JobDetailScreen";
 
 const Stack = createNativeStackNavigator();
 const JobStack = () => {
@@ -10,6 +11,8 @@ const JobStack = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>  
       <Stack.Screen name="JobPosted" component={JobPostedScreen}/>
       <Stack.Screen name="AddJob" component={AddJobScreen}/>
+      <Stack.Screen name="JobDetail" component={JobDetailScreen}/>
+
     </Stack.Navigator>
   );
 };
--- client/screens/ManageJob/JobPostedScreen.tsx
+++ client/screens/ManageJob/JobPostedScreen.tsx
@@ -5,12 +5,7 @@ import { CustomSafeAreaView } from "../../components/common";
 import Bars from "../../navigator/Bar";
 import useUserStore from "../../store/user";
 import shallow from "zustand/shallow";
-import {
-  collection,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import { JobCustom } from "../../model/model";
 
@@ -19,7 +14,9 @@ const JobPostedScreen = ({ navigation }: { navigation: any }) => {
     (state) => [state.user, state.setUser],
     shallow
   );
-  const [jobs, setJobs] = useState([])
+  console.log(user?.id);
+  
+  const [jobs, setJobs] = useState<JobCustom[]>([]);
   useEffect(() => {
     const fetchJobPosted = async () => {
       let jobPosted: any = [];
@@ -32,10 +29,12 @@ const JobPostedScreen = ({ navigation }: { navigation: any }) => {
       });
       setJobs(jobPosted);
     };
-    console.log('Fetchhhhh',fetchJobPosted());
-  }, [])
-  console.log('Jobs', jobs);
+    fetchJobPosted();
+  }, []);
+  console.log("Jobs", jobs);
   
+  
+
   return (
     <CustomSafeAreaView className="flex-1 items-center bg-white px-2">
       <Bars
@@ -48,7 +47,7 @@ const JobPostedScreen = ({ navigation }: { navigation: any }) => {
         <View className="w-full">
           <View className="flex justify-between flex-row my-[10px]">
             <Text className="text-[14px]">
-              Hiện tại có <Text>5</Text> công việc
+              Hiện tại có <Text>{jobs.length}</Text> công việc
             </Text>
             <Pressable
               onPress={() => {
@@ -58,297 +57,69 @@ const JobPostedScreen = ({ navigation }: { navigation: any }) => {
               <Text className="text-[14px] font-[700]">Thêm công việc</Text>
             </Pressable>
           </View>
-
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("Detail");
-            }}
-          >
-            <View
-              style={{
-                borderWidth: 3,
-                borderColor: "#6667AB",
-              }}
-              className="bg-white pr-[4px] ml-[4px] mr-[4px] rounded-[10px] mt-[15px]"
-            >
-              <View style={{ flexDirection: "row" }} className="flex">
-                <Image
-                  //   style={{ width: 80, height: 80, borderRadius: 1, margin: 20 }}
-                  className="w-[80px] h-[80px] rounded-[1px] mx-[10px] my-auto"
-                  source={require("../../assets/icons/groove.jpg.png")}
-                />
-                <View style={{ margin: 20, flexDirection: "column" }}>
-                  <Text className="text-[14px] font-[700]">
-                    GROOVE TECHNOLOGY
-                  </Text>
-                  <Text className="text-[14px] font-[400] text-[#6667AB] mr-[4px]">
-                    SENIOR JAVA DEVELOPER
-                  </Text>
-                  <View style={{ flexDirection: "row", marginTop: 20 }}>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Java
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Spring
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      COBOL
-                    </Text>
+          <View>
+            {jobs.map((job) => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate("JobDetail", {job: job});
+                }}
+              >
+                <View
+                  style={{
+                    borderWidth: 3,
+                    borderColor: "#6667AB",
+                  }}
+                  className="bg-white pr-[4px] ml-[4px] mr-[4px] rounded-[10px] mt-[15px]"
+                >
+                  <View style={{ flexDirection: "row" }} className="flex">
+                    <Image
+                      //   style={{ width: 80, height: 80, borderRadius: 1, margin: 20 }}
+                      // style = {{borderRadius:40}}
+                      className="w-[80px] h-[80px] rounded-[10px] mx-[10px] my-auto"
+                      source={{ uri: job.urlPhoto }}
+                    />
+                    <View style={{ margin: 20, flexDirection: "column" }}>
+                      <Text className="text-[14px] font-[700]">
+                        {job.businessName}
+                      </Text>
+                      <Text className="text-[14px] font-[400] text-[#6667AB] mr-[4px]">
+                        {job.jobTitle}
+                      </Text>
+                      <View style={{ flexDirection: "row", marginTop: 20 }}>
+                        <Text
+                          style={{
+                            marginRight: 10,
+                            backgroundColor: "#BEBEBE",
+                            padding: 5,
+                          }}
+                        >
+                          Java
+                        </Text>
+                        <Text
+                          style={{
+                            marginRight: 10,
+                            backgroundColor: "#BEBEBE",
+                            padding: 5,
+                          }}
+                        >
+                          Spring
+                        </Text>
+                        <Text
+                          style={{
+                            marginRight: 10,
+                            backgroundColor: "#BEBEBE",
+                            padding: 5,
+                          }}
+                        >
+                          COBOL
+                        </Text>
+                      </View>
+                    </View>
                   </View>
                 </View>
-              </View>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("DetailMore");
-            }}
-          >
-            <View
-              style={{
-                borderWidth: 3,
-                borderColor: "#6667AB",
-              }}
-              className="bg-white pr-[4px] ml-[4px] mr-[4px] rounded-[10px] mt-[15px]"
-            >
-              <View style={{ flexDirection: "row" }} className="flex">
-                <Image
-                  //   style={{ width: 80, height: 80, borderRadius: 1, margin: 20 }}
-                  className="w-[80px] h-[80px] rounded-[1px] mx-[10px] my-auto"
-                  source={require("../../assets/icons/groove.jpg.png")}
-                />
-                <View style={{ margin: 20, flexDirection: "column" }}>
-                  <Text className="text-[14px] font-[700]">
-                    GROOVE TECHNOLOGY
-                  </Text>
-                  <Text className="text-[14px] font-[400] text-[#6667AB] mr-[4px]">
-                    SENIOR JAVA DEVELOPER
-                  </Text>
-                  <View style={{ flexDirection: "row", marginTop: 20 }}>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Java
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Spring
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      COBOL
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("DetailMore");
-            }}
-          >
-            <View
-              style={{
-                borderWidth: 3,
-                borderColor: "#6667AB",
-              }}
-              className="bg-white pr-[4px] ml-[4px] mr-[4px] rounded-[10px] mt-[15px]"
-            >
-              <View style={{ flexDirection: "row" }} className="flex">
-                <Image
-                  //   style={{ width: 80, height: 80, borderRadius: 1, margin: 20 }}
-                  className="w-[80px] h-[80px] rounded-[1px] mx-[10px] my-auto"
-                  source={require("../../assets/icons/groove.jpg.png")}
-                />
-                <View style={{ margin: 20, flexDirection: "column" }}>
-                  <Text className="text-[14px] font-[700]">
-                    GROOVE TECHNOLOGY
-                  </Text>
-                  <Text className="text-[14px] font-[400] text-[#6667AB] mr-[4px]">
-                    SENIOR JAVA DEVELOPER
-                  </Text>
-                  <View style={{ flexDirection: "row", marginTop: 20 }}>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Java
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Spring
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      COBOL
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("DetailMore");
-            }}
-          >
-            <View
-              style={{
-                borderWidth: 3,
-                borderColor: "#6667AB",
-              }}
-              className="bg-white pr-[4px] ml-[4px] mr-[4px] rounded-[10px] mt-[15px]"
-            >
-              <View style={{ flexDirection: "row" }} className="flex">
-                <Image
-                  //   style={{ width: 80, height: 80, borderRadius: 1, margin: 20 }}
-                  className="w-[80px] h-[80px] rounded-[1px] mx-[10px] my-auto"
-                  source={require("../../assets/icons/groove.jpg.png")}
-                />
-                <View style={{ margin: 20, flexDirection: "column" }}>
-                  <Text className="text-[14px] font-[700]">
-                    GROOVE TECHNOLOGY
-                  </Text>
-                  <Text className="text-[14px] font-[400] text-[#6667AB] mr-[4px]">
-                    SENIOR JAVA DEVELOPER
-                  </Text>
-                  <View style={{ flexDirection: "row", marginTop: 20 }}>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Java
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Spring
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      COBOL
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate("DetailMore");
-            }}
-          >
-            <View
-              style={{
-                borderWidth: 3,
-                borderColor: "#6667AB",
-              }}
-              className="bg-white pr-[4px] ml-[4px] mr-[4px] rounded-[10px] mt-[15px]"
-            >
-              <View style={{ flexDirection: "row" }} className="flex">
-                <Image
-                  //   style={{ width: 80, height: 80, borderRadius: 1, margin: 20 }}
-                  className="w-[80px] h-[80px] rounded-[1px] mx-[10px] my-auto"
-                  source={require("../../assets/icons/groove.jpg.png")}
-                />
-                <View style={{ margin: 20, flexDirection: "column" }}>
-                  <Text className="text-[14px] font-[700]">
-                    GROOVE TECHNOLOGY
-                  </Text>
-                  <Text className="text-[14px] font-[400] text-[#6667AB] mr-[4px]">
-                    SENIOR JAVA DEVELOPER
-                  </Text>
-                  <View style={{ flexDirection: "row", marginTop: 20 }}>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Java
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      Spring
-                    </Text>
-                    <Text
-                      style={{
-                        marginRight: 10,
-                        backgroundColor: "#BEBEBE",
-                        padding: 5,
-                      }}
-                    >
-                      COBOL
-                    </Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </TouchableOpacity>
+              </TouchableOpacity>
+            ))}
+          </View>
         </View>
       </ScrollView>
     </CustomSafeAreaView>
